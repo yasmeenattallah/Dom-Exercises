78efebb23ae4335adac1dce690dfe24dc17debdf
yasmeenattallah <yattallah7@gmail.com>
2023-06-18 10:22:17 +0300

    sol

---
+++ main.js
@@ -0,0 +1,43 @@
+// const radiusInput = document.getElementById('radius');
+// const btn = document.getElementById('submit');
+// const volume = document.getElementById('volume');
+// const form = document.getElementById("MyForm");
+
+
+// btn.addEventListener('click',(e) => {
+// e.preventDefault();
+// console.log(e);
+// const radius  = parseFloat(radiusInput.value);
+// console.log(typeof radiusInput.value , 111, radius);
+// let sphere_volume = (4/3) * Math.PI * Math.pow(radius, 3); 
+// volume.value = sphere_volume.toFixed(2);
+// });
+//   function calculateVolume() {
+//       const radius = parseFloat(radiusInput.value);
+//       const sphere_volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+//       volume.value = sphere_volume.toFixed(2); // Round to 2 decimal places
+//     }
+
+//     const form = document.getElementById("MyForm");
+//     form.addEventListener("submit", function (event) {
+//       event.preventDefault(); // Prevent form submission
+//       calculateVolume();
+//     });
+
+
+
+// window.localStorage.setItem("name", "yasmeen");
+
+// console.log(window.localStorage);
+// console.log(typeof window.localStorage);
+
+
+// window.localStorage.color = 'red'; 
+// window.localStorage["age"] = 25;
+
+// const person = {
+//     name: "Mohammed",
+//     location: "Gaza",
+// }
+
+// window.localStorage.setItem('user', JSON.stringify(person));
